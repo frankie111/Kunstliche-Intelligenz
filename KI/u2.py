62dc023f37936be344921cfa738b98b4105c3085
mircea <mircea.rautoiu@gmail.com>
2024-02-27 23:17:00 +0200

    astar

--- KI/u2.py
+++ KI/u2.py
@@ -1,9 +1,11 @@
-import numpy as np
+import heapq
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def distance(x, y):
-    return ((x[0] - y[0])) ** 2 + ((x[1] - y[1]) ** 2)
+    return (x[0] - y[0]) ** 2 + ((x[1] - y[1]) ** 2)
 
 
 class Map:
@@ -27,9 +29,6 @@ class Map:
             if m[x, y + 1] == 0:
                 nb = nb + [(x, y + 1)]
         return nb
-
-
-import heapq
 
 
 class PriorityQueue:
@@ -60,56 +59,80 @@ class Stack:
         return self.elements.pop()
 
 
-def dfs(map: Map, start, goal):
-    frontier = Stack()  # Der Stack für die noch zu besuchenden Knoten
-    frontier.put(start)
-    came_from = {start: None}  # Dictionary, um den Vorgänger jedes Knotens zu speichern
+# Hilfsfunktion zur Rekonstruktion des Pfades
+def make_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:  # Rückverfolgung vom Ziel zum Start
+        path.append(current)
+        current = came_from[current]
+    path.append(start)
+    path.reverse()  # Umkehrung des Pfades, um ihn von Start zu Ziel zu ordnen
+    return path
 
-    while not frontier.empty():
-        current = frontier.get()
+
+def dfs(map: Map, start, goal):
+    stack = Stack()
+    stack.put(start)
+    came_from = {start: None}
+
+    while not stack.empty():
+        current = stack.get()
 
         # Ziel gefunden
         if current == goal:
             break
 
         for next in map.neighbors(current):
-            # Wenn der Nachbarknoten noch nicht besucht wurde, fügen Sie ihn dem Stack hinzu
+            # Wenn der Nachbarknoten noch nicht besucht wurde, füge es in dem Stack hinzu
             if next not in came_from:
-                frontier.put(next)
-                came_from[next] = current  # Speichern Sie den Vorgänger
+                stack.put(next)
+                came_from[next] = current  # Speichere den Vorgänger
 
-    return came_from
-
-
-# Hilfsfunktion zur Rekonstruktion des Pfades
-def reconstruct_path(came_from, start, goal):
-    current = goal
-    path = []
-    while current != start:  # Rückverfolgung vom Ziel zum Start
-        path.append(current)
-        current = came_from[current]
-    path.append(start)  # Optional: Füge den Startpunkt hinzu
-    path.reverse()  # Umkehrung des Pfades, um ihn von Start zu Ziel zu ordnen
-    return path
+    return make_path(came_from, start, goal)  # Gib den Weg zurück
 
 
-def draw_path(path, start, goal):
+def astar(map: Map, start, goal):
+    def heuristic(a):
+        return distance(a, goal)
+
+    p_queue = PriorityQueue()  # Startpunkt hat Priorität 0
+    p_queue.put(start, 0)
+    came_from = {start: None}  # Vorgänger
+    cost = {start: 0}  # Kosten von Start bis zu jedem Knoten
+
+    while not p_queue.empty():
+        current = p_queue.get()
+
+        if current == goal:
+            break  # Ziel gefunden
+
+        for next in map.neighbors(current):
+            new_cost = cost[current] + distance(current, next)
+            if next not in cost or new_cost < cost[next]:
+                cost[next] = new_cost
+                priority = new_cost + heuristic(next)  # Priorität mit Heuristik aktualisieren
+                p_queue.put(next, priority)
+                came_from[next] = current  # Pfad aktualisieren
+
+    return make_path(came_from, start, goal)
+
+
+def draw_path(search_alg, map, start, goal):
+    path = search_alg(map, start, goal)
     print(path)
 
-    # Pfad rekonstruieren
-    reconstructed_path = reconstruct_path(path, start, goal)
+    # Visualization of the map with the path
+    fig, ax = plt.subplots()
+    ax.imshow(map.m, cmap='Purples', interpolation='nearest')
+    x_coords, y_coords = zip(*path)
+    ax.plot(y_coords, x_coords, color='black', linewidth=3)  # Visualize path
+    ax.scatter(y_coords[0], x_coords[0], color='red', label='Start', s=250)  # Mark start point
+    ax.scatter(y_coords[-1], x_coords[-1], color='green', label='Goal', s=250)  # Mark goal point
+    ax.legend()
 
-    # Visualisierung der Karte mit dem Pfad
-    plt.imshow(m, cmap='Purples')
-    x_coords, y_coords = zip(*reconstructed_path)
-    plt.plot(y_coords, x_coords, color='yellow', linewidth=3)  # Pfad visualisieren
-    plt.scatter(y_coords[0], x_coords[0], color='red', label='Start')  # Startpunkt markieren
-    plt.scatter(y_coords[-1], x_coords[-1], color='green', label='Goal')  # Zielpunkt markieren
-    plt.legend()
+    ax.set_title(search_alg.__name__, fontsize=24)
     plt.show()
-
-    # Ausgabe des rekonstruierten Pfads
-    # reconstructed_path
 
 
 m = np.array(
@@ -121,8 +144,9 @@ m = np.array(
      [0, 0, 0, 1, 1, 0, 0]])
 mm = Map(m)
 mm.neighbors((4, 1))
-start = (0, 0)
-goal = (0, 6)
-path = dfs(mm, start, goal)
 
-draw_path(path, start, goal)
+_start = (0, 0)
+_goal = (5, 6)
+
+draw_path(dfs, mm, _start, _goal)
+draw_path(astar, mm, _start, _goal)
